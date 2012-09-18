1c3c5ac61e24c4a93b557ac66ccee85c22d4cba8
rahul <rahulsidhu@gmail.com>
2012-09-18 21:59:28 +0530

    changes to jquery.sheet

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   
-  $('#sheet').sheet({
-    buildSheet: '5x2',
-    urlMenu: 'libs/jquery.sheet/sheets/menu.html'
-  });
+  $('#editor')
+    .lyvewrite()
+    .prepend("<div id='sheet' class='jQuerySheet' style='height: 450px;'></div>")
+    .find($('.jQuerySheet'))
+    .sheet({
+      buildSheet: '5x1',
+      urlMenu: 'libs/jquery.sheet/sheets/menu.html'
+    })
+    .end()
+    .append("<div> Some text </div>");
   
 });
--- public/libs/jquery.lyvewrite.js
+++ public/libs/jquery.lyvewrite.js
@@ -2,6 +2,14 @@
 (function($) {
   
   $.fn.lyvewrite = function() {
+
+    return this.each(function() {
+      
+      $(this)
+	.attr('contenteditable', true)
+	.css({'height':600, 'width': 800});
+
+    });
     
   };
 
