4ec4b23168ba5acf0142a91b46cfee6a274c9134
rahul <rahulsidhu@gmail.com>
2012-09-30 01:01:37 +0530

    need a getContent function

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,41 @@
 $(document).ready(function () {
 
+  var Post = Backbone.Model.extend({
+    defaults: {
+      title: "Post title",
+      content: "some words"
+    }
+  });
+
+  var Blog = Backbone.Collection.extend({
+    model: Post
+  });
+
   var WriteView = Backbone.View.extend({
     el: $('.container'),
+
+    template: $('#editortemplate'),
+
+    events: {
+      'click button#save': 'savePost'
+    },
     
     initialize: function () {
       _.bindAll(this);
+      this.model = new Post();
       this.render();
     },
 
     render: function () {
-      $(this.el).append($('#editortemplate').html());
+      $(this.el).append(this.template.html());
       $('#editor').wordframe({
 	width: 600,
 	height: 300
       });
+    },
+
+    savePost: function () {
+      
     }
   });
 
